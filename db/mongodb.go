1afbc80bd8171e9caff2a445783c463cb351687b
Mahmud Ridwan <m@hjr265.me>
2016-07-06 04:07:40 +0600

    Integrate with mgo

---
+++ db/mongodb.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Session struct {
+	*mgo.Session
+}
+
+type PreCreate interface {
+	PreCreate()
+}
+
+type PreModify interface {
+	PreModify()
+}
+
+func Open(url string) (*Session, error) {
+	sess, err := mgo.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+	return &Session{sess}, nil
+}
+
+func callPutHooks(v interface{}, create bool) {
+	if create {
+		d, ok := v.(PreCreate)
+		if ok {
+			d.PreCreate()
+		}
+	} else {
+		d, ok := v.(PreModify)
+		if ok {
+			d.PreModify()
+		}
+	}
+}
+
+func put(sess *Session, col string, v interface{}, id bson.ObjectId) error {
+	_, err := sess.DB("").C(col).UpsertId(id, v)
+	return err
+}

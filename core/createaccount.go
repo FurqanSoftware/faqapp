40aa3aa12ae2ce2daf9cd1a4b1b146a3239e2bb3
Mahmud Ridwan <m@hjr265.me>
2016-07-06 04:09:23 +0600

    Implement create account action

--- core/createaccount.go
+++ core/createaccount.go
@@ -1,4 +1,27 @@
 package core
 
+import "git.furqan.io/faqapp/faqapp/db"
+
 type CreateAccount struct {
+	Handle   string
+	Password string
+
+	AccountRepo db.Accounts
+}
+
+func (a CreateAccount) Validate() error {
+	if a.Handle == "" {
+		return ValidationError{"CreateAccount", "Handle", IssueMissing}
+	}
+	if len(a.Handle) > 14 {
+		return ValidationError{"CreateAccount", "Handle", IssueTooLong}
+	}
+	if len(a.Password) < 8 {
+		return ValidationError{"CreateAccount", "Password", IssueTooShort}
+	}
+	return nil
+}
+
+func (a CreateAccount) Do() (Result, error) {
+	return nil, nil
 }

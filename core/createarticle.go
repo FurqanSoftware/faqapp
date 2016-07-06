04276d9e28e8cfda541d93157f2a6909574aafa4
Mahmud Ridwan <m@hjr265.me>
2016-07-06 23:27:25 +0600

    Implement create article action

--- core/createarticle.go
+++ core/createarticle.go
@@ -1,4 +1,62 @@
 package core
 
+import (
+	"git.furqan.io/faqapp/faqapp/data"
+	"git.furqan.io/faqapp/faqapp/db"
+	"gopkg.in/mgo.v2/bson"
+)
+
 type CreateArticle struct {
+	CategoryID string
+	Title      string
+	Slug       string
+	Order      int
+	Content    string
+
+	ArticleStore  db.ArticleStore
+	CategoryStore db.CategoryStore
+}
+
+func (a CreateArticle) Validate() error {
+	if !bson.IsObjectIdHex(a.CategoryID) {
+		return ValidationError{"CreateArticle", "CategoryID", IssueInvalid}
+	}
+	if a.Title == "" {
+		return ValidationError{"CreateArticle", "Title", IssueMissing}
+	}
+	if len(a.Title) > 256 {
+		return ValidationError{"CreateArticle", "Title", IssueTooLong}
+	}
+	if a.Slug == "" {
+		return ValidationError{"CreateArticle", "Slug", IssueMissing}
+	}
+	if len(a.Slug) > 256 {
+		return ValidationError{"CreateArticle", "Slug", IssueTooLong}
+	}
+	return nil
+}
+
+func (a CreateArticle) Do() (res Result, err error) {
+	cat, err := a.CategoryStore.Get(bson.ObjectIdHex(a.CategoryID))
+	if err != nil {
+		return nil, DatabaseError{"CreateArticle", err}
+	}
+
+	art := data.Article{
+		CategoryID: cat.ID,
+		Title:      a.Title,
+		Slug:       a.Slug,
+		Order:      a.Order,
+	}
+	err = a.ArticleStore.Put(&art)
+	if err != nil {
+		return nil, DatabaseError{"CreateArticle", err}
+	}
+	return CreateArticleRes{
+		Article: &art,
+	}, nil
+}
+
+type CreateArticleRes struct {
+	Article *data.Article
 }

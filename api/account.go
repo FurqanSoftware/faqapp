a01d4b09bf8be526c22536572e6ba8b299957d2d
Mahmud Ridwan <m@hjr265.me>
2016-07-06 16:57:21 +0600

    Implement fetch account list logic

--- api/account.go
+++ api/account.go
@@ -3,6 +3,7 @@ package api
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"git.furqan.io/faqapp/faqapp/core"
 	"git.furqan.io/faqapp/faqapp/db"
@@ -12,14 +13,38 @@ type ServeAccountList struct {
 	AccountRepo db.Accounts
 }
 
+type ServeAccountListRes []ServeAccountListResItem
+
+type ServeAccountListResItem struct {
+	ID         string    `json:"id"`
+	Handle     string    `json:"handle"`
+	FirstIP    string    `json:"first_ip"`
+	RecentIP   string    `json:"recent_ip"`
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
+}
+
 func (h ServeAccountList) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	res, err := core.Do(core.FetchAccountList{})
+	res, err := core.Do(core.FetchAccountList{
+		AccountRepo: h.AccountRepo,
+	})
 	if err != nil {
 		log.Print("fetch account list:", err)
 		ServeInternalServerError(w, r)
 	}
 
-	ServeResult(w, r, res)
+	resp := ServeAccountListRes{}
+	for _, acc := range res.(core.FetchAccountListRes).Accounts {
+		resp = append(resp, ServeAccountListResItem{
+			ID:         acc.ID.Hex(),
+			Handle:     acc.Handle,
+			FirstIP:    acc.FirstIP,
+			RecentIP:   acc.RecentIP,
+			CreatedAt:  acc.CreatedAt,
+			ModifiedAt: acc.ModifiedAt,
+		})
+	}
+	ServeResult(w, r, resp)
 }
 
 type CreateAccountVal struct {
@@ -40,8 +65,9 @@ func (h CreateAccount) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	res, err := core.Do(core.CreateAccount{
-		Handle:   body.Handle,
-		Password: body.Password,
+		Handle:      body.Handle,
+		Password:    body.Password,
+		AccountRepo: h.AccountRepo,
 	})
 	if err != nil {
 		log.Print("create account:", err)
--- core/fetchaccountlist.go
+++ core/fetchaccountlist.go
@@ -1,11 +1,26 @@
 package core
 
-import "git.furqan.io/faqapp/faqapp/db"
+import (
+	"math"
+
+	"git.furqan.io/faqapp/faqapp/data"
+	"git.furqan.io/faqapp/faqapp/db"
+)
 
 type FetchAccountList struct {
 	AccountRepo db.Accounts
 }
 
 func (a FetchAccountList) Do() (Result, error) {
-	return nil, nil
+	accs, err := a.AccountRepo.List(0, math.MaxInt32)
+	if err != nil {
+		return nil, DatabaseError{"FetchAccountList", err}
+	}
+	return FetchAccountListRes{
+		Accounts: accs,
+	}, nil
+}
+
+type FetchAccountListRes struct {
+	Accounts []data.Account
 }
